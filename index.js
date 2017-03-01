545e8d21dd4d956fba6c65385df4c3a1e58548a8
oliverjam <oliverjamesphillips@gmail.com>
2017-03-01 16:51:11 +0000

    Write function to pass failing getInitialTime test

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ function getCurrentSeconds() {
   return date.getSeconds();
 }
 
+var getInitialTime = function() {
+  var initialTime = getCurrentSeconds();
+  return function() {
+    return initialTime;
+  }
+}();
+
 module.exports = {
 	getCurrentSeconds,
+  getInitialTime,
 }
