4c897a7d4b014ca5a8b0909446d0ae3fc190fa7b
oliverjam <oliverjamesphillips@gmail.com>
2017-03-01 16:26:31 +0000

    Remove unnecessary old function

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ function getCurrentSeconds() {
   var date = new Date();
   return date.getSeconds();
 }
-function setsInitialTime(fun) {
-	console.log('hello');
-	return 'hello'
-}
 
 module.exports = {
 	getCurrentSeconds,
-	setsInitialTime
 }
