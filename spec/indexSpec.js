c9f828c8b5fc425017da6ebe43a7a3eb5811f739
oliverjam <oliverjamesphillips@gmail.com>
2017-03-01 16:50:45 +0000

    Create failing test for getInitialTime closure

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,9 +1,15 @@
 describe('Time returning functions', function() {
   var getCurrentSeconds = require('../index.js').getCurrentSeconds;
-    var date = new Date();
-    var seconds = date.getSeconds();
+  var date = new Date()  ;
+  var seconds = date.getSeconds();
   it('Should return current seconds', function() {
     var result = getCurrentSeconds();
     expect(result).toEqual(seconds);
 			})
+  var getInitialTime = require('../index.js').getInitialTime;
+  it('Closure should have old value for initialTime', function() {
+    var initialTime = getInitialTime();
+    var timeNow = setTimeout(getCurrentSeconds, 2000);
+    expect(timeNow).not.toBe(initialTime);
+  })
 })
