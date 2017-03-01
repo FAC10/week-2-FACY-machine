2f5fe9e68a756e31a714bc4c39afce8f52a1847d
Akin909 <akin.sowemimo@gmail.com>
2017-03-01 15:05:05 +0000

    Added a failing test to indexSpec Added dummy failing function to index.js

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ function getCurrentSeconds() {
   var date = new Date();
   return date.getSeconds();
 }
+function setsInitialTime(fun) {
+	console.log('hello');
+	return 'hello'
+}
 
-module.exports = getCurrentSeconds;
+module.exports = {
+	getCurrentSeconds,
+	setsInitialTime
+}
--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,9 +1,17 @@
 describe('Time returning functions', function() {
-  var getCurrentSeconds = require('../index.js');
-  it('Should return current seconds', function() {
+  var getCurrentSeconds = require('../index.js').getCurrentSeconds;
     var date = new Date();
     var seconds = date.getSeconds();
+  it('Should return current seconds', function() {
     var result = getCurrentSeconds();
     expect(result).toEqual(seconds);
+			})
+			it('Converts current seconds to zero',function() {
+				var setsInitialTime = require('../index.js').setsInitialTime;
+				var result = setsInitialTime();
+				expect(typeof result).toBe('number')
+				expect(result).toBe(0);
   })
 })
+
+
