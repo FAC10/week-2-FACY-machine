8c9acee92c609f253205ee8976576c852f804ea4
oliverjam <oliverjamesphillips@gmail.com>
2017-03-01 16:11:08 +0000

    Fix test indentation and add TODO comment

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -6,12 +6,11 @@ describe('Time returning functions', function() {
     var result = getCurrentSeconds();
     expect(result).toEqual(seconds);
 			})
-			it('Converts current seconds to zero',function() {
-				var setsInitialTime = require('../index.js').setsInitialTime;
-				var result = setsInitialTime();
-				expect(typeof result).toBe('number')
-				expect(result).toBe(0);
+	it('Converts current seconds to zero',function() {
+    // Can just return 0 — TODO: improve test
+		var setsInitialTime = require('../index.js').setsInitialTime;
+		var result = setsInitialTime();
+		expect(typeof result).toBe('number')
+		expect(result).toBe(0);
   })
 })
-
-
