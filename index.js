7ac076e71e3d0ba32e5054f2737284f2270e6a5d
Akin909 <akin.sowemimo@gmail.com>
2017-03-01 16:05:09 +0000

    Added musings on pure functional implementation We had a spike re. a pure functional implementation of a counting mechanism ism and added these functions to index.js

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ function getCurrentSeconds() {
   var date = new Date();
   return date.getSeconds();
 }
-function setsInitialTime(fun) {
-	console.log('hello');
-	return 'hello'
+
+function currentTime(){
+  //This is a utility function that is used to generate time
+  return new Date().getSeconds()
 }
+   //This is an immediately invoked function that sets return value to ourTime
+var ourTime = function(){
+//We get the initial time by calling current time then using a closure this values is passed to the returned function below
+  var outsideTime = currentTime()
+
+  return function (){
+    //At the point that this is called current time is now greater than the closed in initial time, the difference between these two number is the elapsed time
+    return currentTime() - outsideTime;
+  } 
+  
+}()
+
 
 module.exports = {
 	getCurrentSeconds,
-	setsInitialTime
+
 }
