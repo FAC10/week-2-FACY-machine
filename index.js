005b8c43e6035f1283f08434f7a0e3f22ebbca70
oliverjam <oliverjamesphillips@gmail.com>
2017-03-02 11:52:45 +0000

    Quickly fix indentation to use editorconfig set-up
    Closes #14

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 function getCurrentSeconds() {
-	var date = new Date();
-	return date.getSeconds();
+    var date = new Date();
+    return date.getSeconds();
 }
 
 var getInitialTime = function() {
-	var initialTime = getCurrentSeconds();
-	return function() {
-		return initialTime;
-	}
+    var initialTime = getCurrentSeconds();
+    return function() {
+        return initialTime;
+    }
 }();
 
 function createDomElement(element) {
-	return document.createElement(element);
+    return document.createElement(element);
 }
 
 function updateDom(element, id, parent) {
-	//Check to see if the element to which we are appending our newly created
-	//element exists
-	parent = parent || document.body
-	var child = createDomElement(element);
-	child.id = id;
-	if (document.querySelector('body')) {
-		parent.appendChild(child);
-	}
-	return checkDomUpdated(id);
+    //Check to see if the element to which we are appending our newly created
+    //element exists
+    parent = parent || document.body
+    var child = createDomElement(element);
+    child.id = id;
+    if (document.querySelector('body')) {
+        parent.appendChild(child);
+    }
+    return checkDomUpdated(id);
 }
 
 function checkDomUpdated(id) {
-	//If the DOM is updated return a boolean value
-	var el = document.getElementById(id)
-	console.log(el);
-	if (el) {
-		return true;
-	} else {
-		return false;	
-	}
+    //If the DOM is updated return a boolean value
+    var el = document.getElementById(id)
+    console.log(el);
+    if (el) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 module.exports = {
-	getCurrentSeconds,
-	getInitialTime,
-	createDomElement
+    getCurrentSeconds,
+    getInitialTime,
+    createDomElement
 }
