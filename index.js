855341924de9df24b0df0f7f4a9a0fbf22bc8921
Akin909 <akin.sowemimo@gmail.com>
2017-03-02 11:02:39 +0000

    Created tests for determining if element is created in the DOM
    Added a test for whether our utility dom creation function produces
    a desired element as well as a testing function within our js with
    access to the DOM can check it

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 function getCurrentSeconds() {
-  var date = new Date();
-  return date.getSeconds();
+	var date = new Date();
+	return date.getSeconds();
 }
 
 var getInitialTime = function() {
-  var initialTime = getCurrentSeconds();
-  return function() {
-    return initialTime;
-  }
+	var initialTime = getCurrentSeconds();
+	return function() {
+		return initialTime;
+	}
 }();
+
+function createDomElement(element) {
+	return document.createElement(element);
+}
+
+function domUpdated(element, id, parent) {
+	//Check to see if the element to which we are appending our newly created
+	//element exists
+	parent = parent || document.body
+	var child = createDomElement(element);
+	child.id = id;
+	if (document.querySelector(parent)) {
+		parent.appendChild(child);
+	}
+	checkDomUpdated(id);
+}
+
+function checkDomUpdated(id) {
+	//If the DOM is updated return a boolean value
+	document.getElementById(id) ? true : false;
+}
 
 module.exports = {
 	getCurrentSeconds,
-  getInitialTime,
+	getInitialTime,
+	createDomElement
 }
--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,25 +1,29 @@
 describe('Time returning functions', function() {
-	var timeObject = require('../index.js')
+	var stopwatch = require('../index.js')
   var getCurrentSeconds = require('../index.js').getCurrentSeconds;
   var date = new Date()  ;
   var seconds = date.getSeconds();
+
+
+
   it('Should return current seconds', function() {
     var result = getCurrentSeconds();
     expect(result).toEqual(seconds);
 			})
-  var getInitialTime = require('../index.js').getInitialTime;
   it('Closure should have old value for initialTime', function() {
+  var getInitialTime = stopwatch.getInitialTime;
     var initialTime = getInitialTime();
     var timeNow = setTimeout(getCurrentSeconds, 2000);
     expect(timeNow).not.toBe(initialTime);
   })
+	// We cannot test the DOM as in testing the framework
 	it('The function should create a DOM Element',function() {
-		var createStopWatch = timeObject.createStopWatch;
-		var element = createStopWatch();
-		expect(element).toBe(<div></div>)	
+		var createDomElement = stopwatch.createDomElement;
+		var element = "div"
+		expect(createDomElement(element)).toBe(`<${element}></${element}>`)	
 	})
 	it('Should update the DOM with our current time',function() {
-		var initialTime = getInitialTime();
+		var initialTime = stopwatch.getInitialTime();
 		
 	})
 })
