8b0e8ff927ba6f531175cc0c2ce210f760fe092d
Akin909 <akin.sowemimo@gmail.com>
2017-03-03 12:36:24 +0000

    Add commenting to JS and JS spec files

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 //Time generating functions
+//This function is a utiliy function that gets and returns the current time
 function getCurrentTime() {
     var date = new Date();
     return date.getTime();
 }
 
-var getElapsedTime = function() {
+///////////////////////////////////////////////////////////////////
+//           Elapsed Time counter
+////////////////////////////////////////////////////////////////////
+
+// This function is our key one it closes in our initial time at the point of
+// invocation which is immediate it then gets called lower down and at that
+// point a call of get current time will give a different time
+var getElapsedTime = (function() {
     var initialTime = getCurrentTime();
     return function() {
         return getCurrentTime() - initialTime;
     }
-}();
+}());
 
+///////////////////////////////////////////////////////////////////
+//          Time Conversion Function
+////////////////////////////////////////////////////////////////////
+//These functions convert the elapsed time from milleseconds to the mins,
+//deciseconds and seconds
 function mins(elapsedTime) {
     var mins = Math.floor(elapsedTime / (1000 * 60))
 
@@ -34,15 +47,28 @@ function seconds(elapsedTime) {
     return seconds
 }
 
-function milliseconds(elapsedTime) {
-    var milliseconds = (elapsedTime.toString()).slice(-3, -1);
-    return milliseconds
+function deciseconds(elapsedTime) {
+    var deciseconds = Math.floor(elapsedTime/100)
+    if (deciseconds >= 100) {
+        deciseconds = deciseconds % 100;
+    }
+    if (deciseconds < 10) {
+        return '0' + deciseconds;
+    }
+    return deciseconds
 }
-
+///////////////////////////////////////////////////////////////////
+//         Animate the Clock
+////////////////////////////////////////////////////////////////////
+// The following function uses request animation frame a native JS function to
+// redraw our changing time variables (this tries to redraw these at a rate of
+// 60FPS (frames per second)
 //Drawing functions
 function drawClock() {
     var elapsedTime = getElapsedTime()
-    updateDom('js-number', mins(elapsedTime), seconds(elapsedTime), milliseconds(elapsedTime));
+    updateDom('js-number', mins(elapsedTime), seconds(elapsedTime), deciseconds(elapsedTime));
+    //This function is used recursively i.e. startAnimation is called below
+    //then recalled again within this function
     var FrameID = requestAnimationFrame(drawClock)
 
 }
@@ -51,7 +77,7 @@ function startAnimation(argument) {
     requestAnimationFrame(drawClock)
 }
 
-//Stop and Start functions
+// Stop and Start functions
 var togglePause = function(bool) {
     var isPaused = bool;
     isPaused = !isPaused;
@@ -59,37 +85,25 @@ var togglePause = function(bool) {
         return isPaused;
     }
 }
-
-//DOM Manipulation and checking functions
-function createDomElement(element) {
-    return document.createElement(element);
-}
-
-function updateDom(id, mins, seconds, milliseconds) {
+//This function takes in the id of our clock div and fills it with our time
+//variables
+function updateDom(id, mins, seconds, deciseconds) {
     //Check to see if the element to which we are js-numberending our newly created
     //element exists
-    var timeElement = document.querySelector('#js-number');
-    timeElement.innerText = `${mins}:${seconds}:${milliseconds}`;
+    var timeElement = document.getElementById(id);
+    timeElement.innerText = `${mins}:${seconds}:${deciseconds}`;
 }
 
-//Cannot update the DOM
-// function checkDomUpdated(id) {
-//     //If the DOM is updated return a boolean value
-//     var el = document.getElementById(id)
-//     console.log(el);
-//     if (el) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
 
 document.getElementById('js-start').addEventListener('click', startAnimation);
 
+//Using Jasmine requires that we export each of our functions as properties of
+//an object which we require/import to be used in Jasmine
 module.exports = {
     getCurrentTime,
     getElapsedTime,
-    createDomElement,
-    togglePause,
-    updateDom
+    updateDom,
+    mins,
+    deciseconds,
+    seconds
 }
